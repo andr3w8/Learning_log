615884b737a75954737087f9ba234e994aa0d0f3
andr3w8 <andi.andrei00@gmail.com>
2022-04-10 16:19:35 +0300

    First commit

---
+++ test.py
@@ -0,0 +1,39 @@
+# if __name__ == '__main__':
+#     n = int(input().strip())
+# if n % 2 == 1:
+#     print("Weird")
+# else:
+#     # n is even
+#     if 2 <= n <= 5:
+#         # n between 2 and 5 inclusive
+#         print("Not Weird")
+#
+#     elif 6 <= n <= 20:
+#         # n between 6 and 20 inclusive
+#         print("Weird")
+#
+#     elif n > 20:
+#         # n greater than 20
+#         print("Not Weird")
+
+
+# if __name__ == '__main__':
+#
+#     a = int(input())
+#     b = int(input())
+#     sum = int(a) + int(b)
+#     diff = int(a) - int(b)
+#     product = int(a) * int(b)
+#
+# print(str(sum))
+# print(str(diff))
+# print(product)
+
+# if __name__ == '__main__':
+#     a = int(input())
+#     b = int(input())
+#     int_div = int(a) // int(b)
+#     flt_div = float(a) / float(b)
+#
+#     print(int_div)
+#     print(flt_div)
